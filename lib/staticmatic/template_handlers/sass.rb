cee1b3646bb335665815279775259cc5e50bbc46
Thomas Reynolds <tdreyno@gmail.com>
2008-06-04 17:07:08 -0700

    pass string to Sass, not template object

--- lib/staticmatic/template_handlers/sass.rb
+++ lib/staticmatic/template_handlers/sass.rb
@@ -2,7 +2,7 @@ module StaticMatic
   module TemplateHandlers
     class Sass < ActionView::TemplateHandler
       def render(template)
-        ::Sass::Engine.new(template).render
+        ::Sass::Engine.new(template.source).render
       end
     end
   end
