cc9dc9bbc95609cb4eb77bdf1b187d5bc3bd2b52
Stephen Bartholomew <steve@curve21.com>
2008-07-03 20:57:42 +0100

    Added build tracking - only builds when templates have been altered, added license

--- lib/staticmatic/builder.rb
+++ lib/staticmatic/builder.rb
@@ -4,59 +4,86 @@ module StaticMatic
     
     def initialize(staticmatic)
       @staticmatic = staticmatic
-      
+      determine_last_build
       build_pages
+      log_version
+    end
+    
+    def determine_last_build
+      versions_file = @staticmatic.root_dir + "/builds"
+      
+      if File.exists?(versions_file)
+        @last_build = File.read(versions_file).split(/\n/)[0]
+      else
+        @last_build = ""
+      end
+    end
+    
+    def log_version
+      timestamp = Time.now.strftime("%Y%m%d%H%M%S")
+      versions_file = @staticmatic.root_dir + "/builds"
+      
+      if File.exists?(versions_file)
+        current_versions = File.read(versions_file)
+        
+        File.open(versions_file, "w") do |file|
+          file.puts timestamp
+          file.puts current_versions
+        end
+      else
+        File.open(versions_file, "w") do |file|
+          file.puts timestamp
+        end
+      end
+      
     end
     
     def build_pages
       ["pages", "stylesheets"].each do |template_path|
         Dir["#{@staticmatic.src_dir}/#{template_path}/**/*"].each do |path|
+
           if File.directory? path
             if !File.exists? build_path_for(path)
-              puts "Creating: #{build_path_for(path)}"
+              @staticmatic.logger.info("Creating: #{build_path_for(path)}")
+              
               FileUtils.mkdir(build_path_for(path))
             end
           else
 
             format = @staticmatic.determine_format_for(path).to_s
-            path = base_template_name_for(path)
+            base_template_name = base_template_name_for(path)
+
             @staticmatic.template.template_format = format
-            if should_overwrite?(path, format)
+            build_file_path = "#{build_path_for(path)}.#{format}"
+
+            if should_overwrite?(path, build_file_path)
               if format == "html"
-                output = @staticmatic.render_with_layout(path)
+                output = @staticmatic.render_with_layout(base_template_name)
               else
-                output = @staticmatic.render(path)
+                output = @staticmatic.render(base_template_name)
               end
                           
               output_prefix = "#{template_path}/" if template_path != "pages"
-              save_built_file(path, output, format)
+              save_built_file(build_file_path, output)
             end
           end
         end
       end
     end
     
-    def should_overwrite?(path, format)
-      return true
+    def should_overwrite?(template_file, build_file)
 
-      build_file = "#{build_path_for(path)}.#{format}"
-
-      path = @staticmatic.full_template_path(path)
-      
       if File.exists? build_file
-
-        template_path = @staticmatic.template.full_template_path(path, @staticmatic.template.finder.pick_template_extension(path))
-        #file_changed? template_path, build_file
-        true
+        file_changed? template_file
       else
         true
       end
     end
     
-    def file_changed?(src_file, build_file)
-      build_modification_time = File.stat(build_file).mtime.strftime("%Y%m%d%H%M%s")
-      template_modification_time = File.stat(src_file).mtime.strftime("%Y%m%d%H%M%s")
-      if template_modification_time > build_modification_time
+    def file_changed?(src_file)
+      template_modification_time = File.stat(src_file).mtime.strftime("%Y%m%d%H%M%S")
+      puts "#{template_modification_time.to_i} > #{@last_build.to_i}"
+      if template_modification_time.to_i > @last_build.to_i
         true
       else
         false
@@ -65,9 +92,11 @@ module StaticMatic
     
     # Strip off src file path and extension
     def base_template_name_for(path)
-      path.gsub!(/^\.\/src\//, '')
-      path.gsub!(/^pages\//, '') if path.match(/^pages/)
-      path.gsub(/\.[a-z]+$/, '')
+      
+      path.gsub("#{@staticmatic.root_dir}/", "").
+           gsub(/^src\//, '').
+           gsub(/^pages\//, '').
+           gsub(/\.[a-z]+$/, '')
     end
     
     # Return an output filename
@@ -76,9 +105,9 @@ module StaticMatic
     end
     
     # Save contents to the specified file with the given extension to the build directory
-    def save_built_file(path, contents, extension)
-      path = "#{build_path_for(path)}.#{extension}"
-      puts "Generating #{path}"
+    def save_built_file(path, contents)
+
+      @staticmatic.logger.info("Generating #{path}")
       File.open(path, 'w+') do |f|
         f << contents
       end
