ea4f6d70c4711844bcc2e58521f78a37254a542e
Thomas Reynolds <tdreyno@gmail.com>
2008-07-31 15:08:52 -0700

    simplify creator

--- lib/staticmatic/creator.rb
+++ lib/staticmatic/creator.rb
@@ -2,16 +2,8 @@ module StaticMatic
   class Creator
     class << self
       def setup(directory)
-        FileUtils.mkdir(directory) unless File.exists?(directory)
-        
-        template_directory = Dir.glob(File.dirname(__FILE__) + "/templates/default/**")
-        
-        FileUtils.cp_r(template_directory, directory)
-        
-        unless File.exists?(directory + "/build")
-          FileUtils.mkdir(directory + "/build")
-          FileUtils.mkdir(directory + "/build/stylesheets")
-        end
+        FileUtils.cp_r(File.dirname(__FILE__) + "/templates/default/.", directory)
+        FileUtils.mkdir_p(directory + "/build/stylesheets")
       end
     end
   end
